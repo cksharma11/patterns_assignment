b9ace92bc89a04975c346eb3c5aa7e23723bcdd9
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-10-21 15:03:07 +0530

    All run_test has been migrated to asserts.

--- run_all_test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-./run_test.sh ./create_triangle.js ./test/input_triangles ./test/expected_out_triangles
-./run_test.sh ./create_rectangle.js ./test/input_rectangles ./test/expected_out_rectangle
-./run_test.sh ./create_diamond.js ./test/input_diamonds ./test/expected_out_diamonds
