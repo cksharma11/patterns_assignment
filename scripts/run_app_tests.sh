7aa1192f66264e471dfb9183c3ece1b89310f2bd
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2018-10-27 17:03:13 +0530

    Add app test data and test scripts

---
+++ scripts/run_app_tests.sh
@@ -0,0 +1,3 @@
+./run_test.sh ../createDiamond.js ../app_test_data/input/diamond_test_inputs ../app_test_data/output/diamond_test_output
+./run_test.sh ../createTriangle.js ../app_test_data/input/triangle_inputs ../app_test_data/output/triangle_outputs
+./run_test.sh ../createRectangle.js ../app_test_data/input/rectangle_inputs ../app_test_data/output/rectangle_outputs
